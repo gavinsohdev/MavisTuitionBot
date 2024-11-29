4a85d2787a86a29da8f3d61f6f011a39908ed2dd
vandergav <gavinsohdev@gmail.com>
2024-11-29 17:18:04 +0800

    Add webhook 9

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const bot = new Telegraf(token);
 app.use(express.json());
 
 app.post("*", async (req, res) => {
+  console.log(`req: ${req.body}`)
   try {
     const chatId = req.body.chatId; // Make sure the request body includes the chat ID
     const message = req.body.message || "Default message"; // Optionally pass a message from the request
